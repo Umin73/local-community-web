95ff961ff539e205f7f03b3bc191fcf065259722
김연희 <gim-yeonhui@gim-yeonhuiui-MacBookAir.local>
2024-07-15 17:03:27 +0900

    initial commit

--- src/pages/MyPost.jsx
+++ src/pages/MyPost.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { myPostData } from "../data/myPostData";
+import Pagination from "react-js-pagination";
 
 export default function MyPost() {
+  const rpp = 1; //한페이지에 5개씩
+  const [currentPost, setCurrentPost] = useState([]); //한페이지에 보여지는 포스트
+  const [page, setPage] = useState(1); //현재페이지
+
+  const handlePageChange = (page) => {
+    setPage(page);
+  };
+
+  const startIndex = (page - 1) * rpp;
+  const lastIndex = rpp * page;
+
+  useEffect(() => {
+    //myPostData.reverse();
+    setCurrentPost(myPostData.slice(startIndex, lastIndex));
+  }, [startIndex, lastIndex, page]);
+
   return (
     <>
       <Mypost>
         <Title>내가 쓴 글</Title>
         <div>
-          {myPostData.map((data) => {
+          {currentPost.map((data) => {
             return (
               <>
                 <Box>
@@ -29,6 +46,15 @@ export default function MyPost() {
               </>
             );
           })}
+          <PgBox>
+            <Pagination
+              activePage={page}
+              itemsCountPerPage={rpp}
+              totalItemsCount={myPostData.length}
+              pageRangeDisplayed={5}
+              onChange={handlePageChange}
+            />
+          </PgBox>
         </div>
       </Mypost>
     </>
@@ -73,4 +99,24 @@ const LikeComment = styled.div`
 const Bottom = styled.div`
   font-size: 13px;
   color: #989898;
+`;
+const PgBox = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+  ul {
+    list-style: none;
+  }
+  li {
+    display: inline-block;
+    width: 20px;
+  }
+  li a {
+    text-decoration: none;
+    color: #484848;
+  }
+
+  li.active a {
+    color: green;
+  }
 `;

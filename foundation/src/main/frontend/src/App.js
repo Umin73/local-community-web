d3baffa59ade6a6d3f6ad325ac4944f95a55c499
hyojean <hrn0314@naver.com>
2024-07-21 13:57:01 +0900

    Merge local-community-frontend/develop

--- foundation/src/main/frontend/src/App.js
+++ foundation/src/main/frontend/src/App.js
@@ -1,20 +1,27 @@
-import React from "react";
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from "react-router-dom";
-
-// Pages
-import MainPage from "./component/page/MainPage";
+import {Route, Routes} from "react-router-dom";
+import MainPage from "./pages/main/MainPage";
+import Login from "./pages/user/Login";
+import SignUp from "./pages/user/SignUp";
+import MyPage from "./pages/my/MyPage";
+import MyInfo from "./pages/my/MyInfo"
+import MyPost from "./pages/my/MyPost";
+import MyComment from "./pages/my/MyComment";
+import Bookmark from "./pages/my/Bookmark";
 
 function App(props) {
     return (
-        <BrowserRouter>
+        <>
             <Routes>
-                <Route index element = {<MainPage />} />
+                <Route path="/" element={<MainPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/myPage" element={<MyPage />} />
+                <Route path="/myInfo" element={<MyInfo />} />
+                <Route path="/myPost" element={<MyPost />} />
+                <Route path="/myComment" element={<MyComment />} />
+                <Route path="/bookmark" element={<Bookmark />} />
             </Routes>
-        </BrowserRouter>
+        </>
     );
 }
 

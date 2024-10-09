e868623fc1ce7057e872974e36c9f09ab5a23aa4
joamksh <joamksh@naver.com>
2024-10-09 22:10:39 +0900

    ref : 추가한 프로필사진, 닉네임 회원가입 및 myinfo 프백 연결

--- frontend/src/pages/my/MyInfo.jsx
+++ frontend/src/pages/my/MyInfo.jsx
@@ -18,8 +18,8 @@ export default function MyPost() {
   useEffect(() => {
     const fetchUserInfo = async () => {
       try {
-        const response = await axios.get('/mypage/user', {
-          withCredentials: true // 쿠키를 포함하여 서버로 요청을 보냄
+        const response = await axios.get("/mypage/user", {
+          withCredentials: true, // 쿠키를 포함하여 서버로 요청을 보냄
         });
         setUserInfo(response.data);
         setNickname(response.data.nickname);
@@ -38,7 +38,7 @@ export default function MyPost() {
     e.preventDefault();
     if (password !== pwdCheck) {
       alert("비밀번호가 일치하지 않습니다.");
-      setPwdCheck('');
+      setPwdCheck("");
     } else {
       alert("변경이 가능합니다.");
     }
@@ -48,7 +48,7 @@ export default function MyPost() {
     e.preventDefault();
     if (nickname === userInfo.nickname) {
       alert("이미 사용중인 닉네임입니다.");
-      setNickname('');
+      setNickname("");
     } else {
       alert("변경이 가능합니다.");
     }
@@ -56,29 +56,35 @@ export default function MyPost() {
 
   const canEdit = async (e) => {
     e.preventDefault();
-    if (window.confirm('정보를 변경하시겠습니까?')) {
+    if (window.confirm("정보를 변경하시겠습니까?")) {
       try {
-        await axios.put(`/mypage/user`, {
-          nickname,
-          phone: phoneNum,
-          address,
-          email,
-          password
-        }, {
-          withCredentials: true // 쿠키를 포함하여 서버로 요청을 보냄
-        });
-        alert('수정되었습니다.');
+        await axios.put(
+            `/mypage/user`,
+            {
+              nickname,
+              phone: phoneNum,
+              address,
+              email,
+              password,
+            },
+            {
+              withCredentials: true, // 쿠키를 포함하여 서버로 요청을 보냄
+            }
+        );
+        alert("수정되었습니다.");
         window.location.reload();
       } catch (error) {
         console.error("Failed to update user info:", error);
-        alert('수정에 실패했습니다.');
+        alert("수정에 실패했습니다.");
       }
     }
   };
 
   return (
       <>
-        <div className="root-wrap"> <Header /></div>
+        <div className="root-wrap">
+          <Header />
+        </div>
         <div className="side-wrap">
           <Sidebar />
         </div>
@@ -91,8 +97,28 @@ export default function MyPost() {
                 <tr>
                   <td className="info-item">프로필 사진</td>
                   <td>
-                    <input type="file" id="newProfilePic" style={{ fontSize: '11px' }} />
-                    <button style={{ fontSize: '11px', padding: '5px' }}>삭제</button>
+                    {/* 프로필 사진을 보여주는 img 태그 */}
+                    <img
+                        src={
+                            userInfo.profile_url ||
+                            "https://image11.coupangcdn.com/image/cmg/oms/banner/e587245b-b580-48da-84b6-ac5f6aa392fb_980x670.jpg"
+                        }
+                        alt="프로필 사진"
+                        style={{
+                          width: "100px",
+                          height: "100px",
+                          borderRadius: "50%",
+                        }}
+                    />
+                    {/* 사용자가 파일을 선택할 수 있는 input */}
+                    <input
+                        type="file"
+                        id="newProfilePic"
+                        style={{ fontSize: "11px", marginLeft: "10px" }}
+                    />
+                    <button style={{ fontSize: "11px", padding: "5px" }}>
+                      삭제
+                    </button>
                   </td>
                 </tr>
                 <tr>
@@ -119,15 +145,23 @@ export default function MyPost() {
                 <tr>
                   <td className="info-item">비밀번호</td>
                   <td>
-                    <input type="password" id="password" value={password}
-                           onChange={(e) => setPassword(e.target.value)} />
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                   </td>
                 </tr>
                 <tr>
                   <td className="info-item">비밀번호 확인</td>
                   <td>
-                    <input type="password" id="pwdCheck" value={pwdCheck}
-                           onChange={(e) => setPwdCheck(e.target.value)} />
+                    <input
+                        type="password"
+                        id="pwdCheck"
+                        value={pwdCheck}
+                        onChange={(e) => setPwdCheck(e.target.value)}
+                    />
                     <NicknameCheck onClick={canChangePwd}>중복확인</NicknameCheck>
                   </td>
                 </tr>
@@ -158,8 +192,13 @@ export default function MyPost() {
                 <tr>
                   <td className="info-item">전화번호</td>
                   <td>
-                    <input type="text" id="phoneNum" value={phoneNum} name="phoneNum"
-                           onChange={(e) => setPhoneNum(e.target.value)} />
+                    <input
+                        type="text"
+                        id="phoneNum"
+                        value={phoneNum}
+                        name="phoneNum"
+                        onChange={(e) => setPhoneNum(e.target.value)}
+                    />
                   </td>
                 </tr>
                 </tbody>
@@ -206,7 +245,6 @@ const NicknameCheck = styled.button`
 // import styled from "styled-components";
 // import "../../css/MyInfo.css";
 // import "../../css/MyPage.css";
-// import { userData } from "../../data/userData";
 // import Header from "../../components/my/Header";
 // import Sidebar from "../../components/my/Sidebar";
 // import axios from "axios";
@@ -220,26 +258,28 @@ const NicknameCheck = styled.button`
 //   const [email, setEmail] = useState("");
 //   const [userInfo, setUserInfo] = useState({});
 //
-//   const userId = 1; // 사용자 ID를 백엔드에서 인증 정보를 통해 얻도록 변경해야 합니다.
-//   const isSamePwd = password === pwdCheck;
-//   const isSameNick = nickname === userData[0].nickname; // 사용자가 입력한 닉네임과 기존 닉네임 비교
-//
 //   useEffect(() => {
-//     // 컴포넌트 마운트 시 사용자 정보를 가져옵니다.
-//     axios.get(`/mypage/user?id=${userId}`)
-//         .then(response => {
-//           setUserInfo(response.data);
-//           setNickname(response.data.nickname);
-//           setPhoneNum(response.data.phone);
-//           setAddress(response.data.address);
-//           setEmail(response.data.email);
-//         })
-//         .catch(error => console.error("Failed to fetch user info:", error));
+//     const fetchUserInfo = async () => {
+//       try {
+//         const response = await axios.get('/mypage/user', {
+//           withCredentials: true // 쿠키를 포함하여 서버로 요청을 보냄
+//         });
+//         setUserInfo(response.data);
+//         setNickname(response.data.nickname);
+//         setPhoneNum(response.data.phone);
+//         setAddress(response.data.address);
+//         setEmail(response.data.email);
+//       } catch (error) {
+//         console.error("Failed to fetch user info:", error);
+//       }
+//     };
+//
+//     fetchUserInfo();
 //   }, []);
 //
 //   const canChangePwd = async (e) => {
 //     e.preventDefault();
-//     if (!isSamePwd) {
+//     if (password !== pwdCheck) {
 //       alert("비밀번호가 일치하지 않습니다.");
 //       setPwdCheck('');
 //     } else {
@@ -249,8 +289,7 @@ const NicknameCheck = styled.button`
 //
 //   const canChangeNick = async (e) => {
 //     e.preventDefault();
-//
-//     if (!isSameNick) {
+//     if (nickname === userInfo.nickname) {
 //       alert("이미 사용중인 닉네임입니다.");
 //       setNickname('');
 //     } else {
@@ -262,13 +301,14 @@ const NicknameCheck = styled.button`
 //     e.preventDefault();
 //     if (window.confirm('정보를 변경하시겠습니까?')) {
 //       try {
-//         await axios.put(`/mypage/user/${userId}`, {
-//           userId,
+//         await axios.put(`/mypage/user`, {
 //           nickname,
 //           phone: phoneNum,
 //           address,
 //           email,
-//           password, // 필요한 경우 암호 필드도 포함
+//           password
+//         }, {
+//           withCredentials: true // 쿠키를 포함하여 서버로 요청을 보냄
 //         });
 //         alert('수정되었습니다.');
 //         window.location.reload();
@@ -404,4 +444,3 @@ const NicknameCheck = styled.button`
 //   font-size: 11px;
 //   padding: 5px;
 // `;
-
--- frontend/src/pages/user/SignUp.jsx
+++ frontend/src/pages/user/SignUp.jsx
@@ -8,7 +8,8 @@ export default function SignUp() {
   const [id, setId] = useState("");
   const [pw, setPw] = useState("");
   const [pwCheck, setPwCheck] = useState("");
-  const [name, setName] = useState("");
+  const [username, setusername] = useState("");  // 이름 상태 값
+  const [nickname, setnickname] = useState("");  // 닉네임 상태 값
   const [address, setAddress] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
@@ -27,11 +28,13 @@ export default function SignUp() {
       userId: id,
       password: pw,
       passwordCheck: pwCheck,
-      username: name,
+      username: username,  // 이름 추가
+      nickname: nickname,  // 닉네임 추가
       address,
       phone,
       email: `${email}@${selected}`,
     };
+
     console.log(signUpData);
 
     try {
@@ -65,7 +68,11 @@ export default function SignUp() {
           </div>
           <div className="signup_inputTitle">이름</div>
           <div className="signup_inputWrap">
-            <input type="text" className="input" value={name} onChange={(e) => setName(e.target.value)} />
+            <input type="text" className="input" value={username} onChange={(e) => setusername(e.target.value)} /> {/* 이름 입력 */}
+          </div>
+          <div className="signup_inputTitle">닉네임</div>
+          <div className="signup_inputWrap">
+            <input type="text" className="input" value={nickname} onChange={(e) => setnickname(e.target.value)} /> {/* 닉네임 입력 */}
           </div>
           <div className="signup_inputTitle">주소 검색</div>
           <div className="addressWrap">
@@ -92,7 +99,8 @@ export default function SignUp() {
           <div className="signup_emailWrap">
             <div className="signup_emailInputWrap">
               <input type="text" className="input" value={email} onChange={(e) => setEmail(e.target.value)} />
-            </div> &nbsp;@&nbsp;
+            </div>
+            &nbsp;@&nbsp;
             <div className="signup_emailList">
               <select className="selectEmailWrap" onChange={handleEmailSelect} value={selected}>
                 {emailList.map((item) => (
@@ -110,6 +118,127 @@ export default function SignUp() {
       </div>
   );
 }
+
+
+// import React, { useState } from "react";
+// import axios from "axios";
+// import "../../css/SignUp.css";
+// import Postcode from "../../components/user/PostCode";
+//
+// export default function SignUp() {
+//   const emailList = ["gmail.com", "naver.com", "hanmail.net", "daum.net"];
+//   const [id, setId] = useState("");
+//   const [pw, setPw] = useState("");
+//   const [pwCheck, setPwCheck] = useState("");
+//   const [userName, setUserName] = useState("");
+//   const [nickName, setNickName] = useState("");
+//   const [address, setAddress] = useState("");
+//   const [phone, setPhone] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [selected, setSelected] = useState(emailList[0]);
+//
+//   const handleAddressChange = (address) => {
+//     setAddress(address);
+//   };
+//
+//   const handleEmailSelect = (e) => {
+//     setSelected(e.target.value);
+//   };
+//
+//   const handleSignUp = async () => {
+//     const signUpData = {
+//       userId: id,
+//       password: pw,
+//       passwordCheck: pwCheck,
+//       userName: userName,
+//       nickName: nickName,
+//       address,
+//       phone,
+//       email: `${email}@${selected}`,
+//     };
+//     console.log(signUpData);
+//
+//     try {
+//       const response = await axios.post("/jwt-login/join", signUpData);
+//       if (response.data === "회원가입 성공") {
+//         window.location.href = "/jwt-login/login";
+//       } else {
+//         console.error("회원가입 실패:", response.data);
+//       }
+//     } catch (error) {
+//       console.error("회원가입 실패:", error);
+//     }
+//   };
+//
+//   return (
+//       <div className="signupPage">
+//         <div className="titleWrap">회원가입</div>
+//         <div className="signup_contentWrap">
+//           <div className="signup_inputTitle">아이디</div>
+//           <div className="signup_inputWrap">
+//             <input type="text" className="input" value={id} onChange={(e) => setId(e.target.value)}/>
+//             <button className="checkButton">중복 확인</button>
+//           </div>
+//           <div className="signup_inputTitle">비밀번호</div>
+//           <div className="signup_inputWrap">
+//             <input type="password" className="input" value={pw} onChange={(e) => setPw(e.target.value)}/>
+//           </div>
+//           <div className="signup_inputTitle">비밀번호 확인</div>
+//           <div className="signup_inputWrap">
+//             <input type="password" className="input" value={pwCheck} onChange={(e) => setPwCheck(e.target.value)}/>
+//           </div>
+//           <div className="signup_inputTitle">이름</div>
+//           <div className="signup_inputWrap">
+//             <input type="text" className="input" value={userName} onChange={(e) => setUserName(e.target.value)}/>
+//           </div>
+//           <div className="signup_inputTitle">닉네임</div>
+//           <div className="signup_inputWrap">
+//             <input type="text" className="input" value={nickName} onChange={(e) => setNickName(e.target.value)}/>
+//           </div>
+//           <div className="signup_inputTitle">주소 검색</div>
+//           <div className="addressWrap">
+//             <div className="signup_addressInputWrap">
+//               <input type="text" className="input" readOnly value={address}/>
+//             </div>
+//             <Postcode onAddressChange={handleAddressChange}/>
+//           </div>
+//           <div className="signup_inputTitle">전화번호</div>
+//           <div>
+//             <div className="signup_phoneInputWrap">
+//               <input type="text" className="input" maxLength={3} onChange={(e) => setPhone(e.target.value)}/>
+//             </div>
+//             &nbsp;-&nbsp;
+//             <div className="signup_phoneInputWrap">
+//               <input type="text" className="input" maxLength={4} onChange={(e) => setPhone(e.target.value)}/>
+//             </div>
+//             &nbsp;-&nbsp;
+//             <div className="signup_phoneInputWrap">
+//               <input type="text" className="input" maxLength={4} onChange={(e) => setPhone(e.target.value)}/>
+//             </div>
+//           </div>
+//           <div className="signup_inputTitle">이메일</div>
+//           <div className="signup_emailWrap">
+//             <div className="signup_emailInputWrap">
+//               <input type="text" className="input" value={email} onChange={(e) => setEmail(e.target.value)}/>
+//             </div>
+//             &nbsp;@&nbsp;
+//             <div className="signup_emailList">
+//               <select className="selectEmailWrap" onChange={handleEmailSelect} value={selected}>
+//                 {emailList.map((item) => (
+//                     <option value={item} key={item}>
+//                       {item}
+//                     </option>
+//                 ))}
+//               </select>
+//             </div>
+//           </div>
+//         </div>
+//         <div>
+//           <button className="joinButton" onClick={handleSignUp}>가입하기</button>
+//         </div>
+//       </div>
+//   );
+// }
 
 
 // import React, { useState } from "react";

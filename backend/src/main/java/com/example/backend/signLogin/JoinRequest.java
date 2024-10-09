a4481c6de8083e7530aa7dac01e0258c02c562e1
joamksh <joamksh@naver.com>
2024-10-09 22:09:11 +0900

    ref : user속성에 프로필 사진, nickname추가

--- backend/src/main/java/com/example/backend/signLogin/JoinRequest.java
+++ backend/src/main/java/com/example/backend/signLogin/JoinRequest.java
@@ -17,20 +17,23 @@ public class JoinRequest {
     @NotBlank(message = "비밀번호가 비어있습니다.")
     private String password;
     private String passwordCheck;
-
+    private String nickname;
     private String username;
     private String address;
     private String phone;
     private String email;
+
 
     public User toEntity() {
         return User.builder()
                 .userId(this.userId)
                 .password(this.password)
                 .username(this.username)
+                .nickname(this.nickname)
                 .address(this.address)
                 .phone(this.phone)
                 .email(this.email)
+                .profile_url("https://image11.coupangcdn.com/image/cmg/oms/banner/e587245b-b580-48da-84b6-ac5f6aa392fb_980x670.jpg") // Default profile picture URL
                 .build();
     }
 
@@ -39,9 +42,11 @@ public class JoinRequest {
                 .userId(this.userId)
                 .password(encodedPassword)
                 .username(this.username)
+                .nickname(this.nickname)
                 .address(this.address)
                 .phone(this.phone)
                 .email(this.email)
+                .profile_url("https://image11.coupangcdn.com/image/cmg/oms/banner/e587245b-b580-48da-84b6-ac5f6aa392fb_980x670.jpg") // Default profile picture URL
                 .build();
     }
 }
